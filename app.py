4d1c359ae2f3ec1701dd8350c6d9be57b93058fe
frankcasanova <frankcasanova.info@gmail.com>
2021-10-25 20:02:17 +0200

    v1

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from werkzeug.wrappers import response
+
+# Intitialise the app
+app = Flask(__name__)
+
+# Define what the app does
+@app.get("/greet")
+def index():
+    """
+    TODO:
+    1. Capture first name & last name
+    2. If either is not provided: respond with an error
+    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
+    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
+    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
+    """
+    fname=request.args.get('fname')
+    lname=request.args.get('lname')
+
+    if not fname and not lname:
+        return jsonify({'status':'error'})
+    elif fname and not lname:
+        return jsonify({'data':f'Hello, {fname}'})    
+    elif lname and not fname:
+        return jsonify({'data':f'Hello, {lname}'})
+    else:
+        response={'data': f'Is your name {fname}{lname}'}    
+        
+    return jsonify(response)
