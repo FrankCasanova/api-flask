2a791c22adb1f550ce5225fa86b3602c2681747c
frankcasanova <frankcasanova.info@gmail.com>
2021-10-26 19:43:20 +0200

    project finished

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify
-from werkzeug.wrappers import response
-from model.dbHandler import match_exact, match_like 
+# dictionary-api-python-flask/app.py
+from flask import Flask, request, jsonify, render_template
+from model.dbHandler import match_exact, match_like
 
 app = Flask(__name__)
+
 
 @app.get("/")
 def index():
@@ -11,46 +12,44 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    response = {'usage': '/dict?=<word>'}
-    
-    return jsonify(response)
+    # response = {"usage": "/dict?=<word>"}
+    # Since this is a website with front-end, we don't need to send the usage instructions
+    return render_template("index.html")
+
 
 @app.get("/dict")
 def dictionary():
     """
-   DEFAULT ROUTE
+    DEFAULT ROUTE
     This method will
     1. Accept a word from the request
-    2. Try to find an exact match and return it if found
+    2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    word = request.args.get('word')
+    words = request.args.getlist("word")
 
-    #return an error querystring is malformed
-    if not word:
-        return jsonify({'status': 'error', 'data': 'word not found'})
+    if not words:
+        response = {"status": "error", "word": words, "data": "word not found"}
+        return jsonify(response)
 
-    #try to find an exact match
-    definition = match_exact(word)
-    if definition:
-        return jsonify({'status': 'success','data': definition })
+    # Initialise the reponse
+    response = {"words": []}
 
-    #try to find an approximate match
-    definitions = match_like(word)
-    if definitions:
-        return jsonify({'status': 'partial','data': definitions})
-    else:
-        return jsonify({'status': 'error','data': 'word not found'})
+    for word in words:
+        # Try to find an exact match
+        definitions = match_exact(word)
+        if definitions:
+            response["words"].append({"status": "success", "word": word, "data": definitions})
+        else:
+            # Try to find an approximate match
+            definitions = match_like(word)
+            if definitions:
+                response["words"].append({"status": "partial", "word": word, "data": definitions})
+            else:
+                response[words].append({"status": "error", "word": word, "data": "word not found"})
 
-        
-        
-
-    
-    
-     
-
-
-
+    # Render the results.html template and return along with response after processing all words
+    return render_template("results.html", response=jsonify(response))
 
 if __name__ == "__main__":
-    app.run()    
+    app.run()
