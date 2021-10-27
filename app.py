3d5d95735ad338dae0b64e0063c20e897581c4cf
frankcasanova <frankcasanova.info@gmail.com>
2021-10-27 17:17:46 +0200

    implementing functionalities

--- app.py
+++ app.py
@@ -1,55 +1,65 @@
 # dictionary-api-python-flask/app.py
-from flask import Flask, request, jsonify, render_template
-from model.dbHandler import match_exact, match_like
+from flask import Flask, request, jsonify
+from werkzeug.utils import send_file
+from bin.filters import apply_filter
+
 
 app = Flask(__name__)
 
+filters_available = [
+    "blur" ,
+    "contour" ,
+    "detail" ,
+    "edge_enhance" ,
+    "edge_enhance_more" ,
+    "emboss" ,
+    "find_edges" ,
+    "sharpen" ,
+    "smooth" ,
+    "smooth_more" ,
+]
 
-@app.get("/")
+@app.get("/", methods=['GET', 'POST'])
 def index():
     """
-    DEFAULT ROUTE
-    This method will
-    1. Provide usage instructions formatted as JSON
+    TODO:
+    1. Return the usage instructions that:
+        a) specifies which filters are avaiable
+        b) specifies and the method format
     """
-    # response = {"usage": "/dict?=<word>"}
-    # Since this is a website with front-end, we don't need to send the usage instructions
-    return render_template("index.html")
+    response = {
+        'filters_available': filters_available,
+        'usage': {'http_method': 'POST', 'URL':'/<filter_available>'},
+    }
+    
+    return jsonify(response)
+
+   
+    
 
 
-@app.get("/dict")
-def dictionary():
+@app.get("/<filter>")
+def image_filter(filter):
     """
-    DEFAULT ROUTE
-    This method will
-    1. Accept a word from the request
-    2. Try to find an exact match, and return it if found
-    3. If not found, find all approximate matches and return
+    TODO:
+    1.Checks if the provided filter is available, if not, return an error.
+    2.Chech if a file has been provided in the POST request, if not return an error. 
+    3.Apply the filter using apply_filter() method from bin.filters
+    4.Return the filtered image as response
     """
-    words = request.args.getlist("word")
-
-    if not words:
-        response = {"status": "error", "word": words, "data": "word not found"}
+    if filter not in filters_available:
+        response = {'error': 'incorrect filter'}
         return jsonify(response)
+    
+    file = request.files['image']
+    if not file:
+        response = {'error': 'no file provided'}
+        return jsonify(response)
+    
+    filtered_image = apply_filter(file, filter)
+    
+    return send_file(filtered_image, mimetype='image/JPEG')
 
-    # Initialise the reponse
-    response = {"words": []}
-
-    for word in words:
-        # Try to find an exact match
-        definitions = match_exact(word)
-        if definitions:
-            response["words"].append({"status": "success", "word": word, "data": definitions})
-        else:
-            # Try to find an approximate match
-            definitions = match_like(word)
-            if definitions:
-                response["words"].append({"status": "partial", "word": word, "data": definitions})
-            else:
-                response[words].append({"status": "error", "word": word, "data": "word not found"})
-
-    # Render the results.html template and return along with response after processing all words
-    return render_template("results.html", response=jsonify(response))
 
 if __name__ == "__main__":
     app.run()
---
+++ bin/filters.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageFilter
+import io
+
+def apply_filter(file: object, filter: str) -> object:
+    """
+    TODO:
+    1.Accept the image as file object, and the filter type as string
+    2.Oopen the as an PIL Image object
+    3.Apply the filter
+    4.Convert the PIL Image object to file object
+    5.Return the file object
+    """
+    pass
