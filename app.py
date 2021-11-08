2f23b420a06fde1eb288785f114d54d991e77a8b
frankcasanova <frankcasanova.info@gmail.com>
2021-11-08 13:52:20 +0100

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, Query
+from fastapi . encoders import jsonable_encoder
+from model . dbHandler import match_exact, match_like
 
 app = FastAPI()
 
 @app.get('/')
 def index():
-    return 'TODO'
+    """
+    DEFAULT ROUTE
+    this method will
+    1. Provide usage instructions formated as JSON
+    """
+    return TODO
+
+@app.get('/dict')
+def dictionary():
+    """
+    DEFAULT ROUTE
+    this method will
+    1. Accept a word form the request
+    2. Try to find an exact match, and return it if found
+    3. If not found, find all approcimate matches and return
+    """
+    return TODO
